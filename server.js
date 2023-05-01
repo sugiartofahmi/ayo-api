1d6e0bd76f023e835c16de6fb9200f1efaeb4d35
sugiartofahmi <sugiartofahmi@gmail.com>
2023-05-01 09:13:45 +0700

    Fahmi Sugiarto | Fix Cannot POST

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import jsonServer from "json-server";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const data = JSON.parse(fs.readFileSync(path.join(__dirname, "db/db.json")));
 const server = jsonServer.create();
-const router = jsonServer.router("db/db.json");
+const router = jsonServer.router(data);
 const middlewares = jsonServer.defaults();
 const port = 3002;
 server.use(middlewares);
